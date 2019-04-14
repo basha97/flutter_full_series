b5f66933c222080f7c692f6522f5df41e85ac497
basha97 <bashaadil2727@gmail.com>
2019-04-14 12:39:04 +0530

    proper conditional statement

--- lib/products.dart
+++ lib/products.dart
@@ -1,32 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-
   final List<String> products;
 
-  Products(this.products){
+  Products(this.products) {
     print('[Products Widget] Constructor');
   }
 
-  Widget _productItem(BuildContext context, int index){
+  Widget _productItem(BuildContext context, int index) {
     return Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(products[index]),
-                  ],
-                ),
-              );
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildproductlist() {
+    Widget productCard;
+
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _productItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No data founds'),
+      );
+    }
+
+    return productCard;
   }
 
   @override
   Widget build(BuildContext context) {
     print('[Products Widget] build()');
-    return products.length > 0 ? ListView.builder(
-      itemBuilder: _productItem,
-      itemCount: products.length,
-    ): Center(
-      child: Text('No products found'),
-    );
+    return _buildproductlist();
   }
 }
