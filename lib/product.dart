911b347157639935654c12b5f26d4aeee2e620e6
basha97 <bashaadil2727@gmail.com>
2019-04-16 13:11:50 +0530

    Some design on page

--- lib/product.dart
+++ lib/product.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget{
+class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('title'),
-          backgroundColor: Colors.greenAccent,
-        ),
-        body: Center(
-          child: Text('On the products page '),
-        ),
-      );
+      appBar: AppBar(
+        title: Text('title'),
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('back'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
