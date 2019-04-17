f970c6c4af944644b5f48fbdf9b8e85b5e31380c
basha97 <bashaadil2727@gmail.com>
2019-04-17 12:52:41 +0530

    Basics authentication

--- lib/products.dart
+++ lib/products.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './product.dart';
+import './pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, String>> products;
