270b935e2267a097d3933f1c5c2ffe7501988111
basha97 <bashaadil2727@gmail.com>
2019-04-19 14:42:10 +0530

    Working with rows

--- lib/products.dart
+++ lib/products.dart
@@ -13,19 +13,30 @@ class Products extends StatelessWidget {
         children: <Widget>[
           Image.asset(products[index]['image']),
           Container(
-            margin: EdgeInsets.all(10.0),
-            child: Text(products[index]['title']),
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  products[index]['title'],
+                  style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Text(
+                  products[index]['price'].toString()
+                ),
+              ],
+            ),
           ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               FlatButton(
-                child: Text('Details '),
-                onPressed: () => Navigator.pushNamed<bool>(
-                    context,'/product/'+ index.toString()
-                    )
-                    ),
-              
+                  child: Text('Details '),
+                  onPressed: () => Navigator.pushNamed<bool>(
+                      context, '/product/' + index.toString())),
             ],
           )
         ],
