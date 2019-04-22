6fb31e8646e1f2affc9e388aae49bb6104b9cf81
basha97 <bashaadil2727@gmail.com>
2019-04-22 15:08:34 +0530

    Closing the keyboard

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -89,26 +89,31 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth + 0.95;
     final double targetPadding = deviceWidth - targetWidth;
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formkey,
-              child: ListView(
-          // padding: EdgeInsets.symmetric(horizontal: targetPadding/2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              child: Text('Save'),
-              color: Theme.of(context).accentColor,
-              textColor: Colors.white,
-              onPressed: _submitForm,
-            )
-          ],
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+          child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formkey,
+                child: ListView(
+             padding: EdgeInsets.all(10.0),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text('Save'),
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              )
+            ],
+          ),
         ),
       ),
     );
