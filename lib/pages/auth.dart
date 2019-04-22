a5e238074c0e6351bcffc31d68e234097256f0ab
basha97 <bashaadil2727@gmail.com>
2019-04-22 14:01:41 +0530

    Adding media queries

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -12,85 +12,88 @@ class _AuthPageState extends State<AuthPage> {
   String _passwordvalue;
   bool _acceptterms = false;
 
-   _buildBackgroundImage(){
+  _buildBackgroundImage() {
     return DecorationImage(
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.5), BlendMode.dstATop),
-                image: AssetImage('assets/bg.jpg'));
+        fit: BoxFit.cover,
+        colorFilter:
+            ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+        image: AssetImage('assets/bg.jpg'));
   }
 
-  Widget _buildEmailTextField(){
+  Widget _buildEmailTextField() {
     return TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    filled: true
-                  ),
-                  onChanged: (String value) {
-                    setState(() {
-                      _emailvalue = value;
-                    });
-                  },
-                );
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(labelText: 'Email', filled: true),
+      onChanged: (String value) {
+        setState(() {
+          _emailvalue = value;
+        });
+      },
+    );
   }
 
-  Widget _buildPasswordTextField(){
+  Widget _buildPasswordTextField() {
     return TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: 'Password',filled: true),
-                  onChanged: (String value) {
-                    setState(() {
-                      _passwordvalue = value;
-                    });
-                  },
-                );
+      obscureText: true,
+      decoration: InputDecoration(labelText: 'Password', filled: true),
+      onChanged: (String value) {
+        setState(() {
+          _passwordvalue = value;
+        });
+      },
+    );
   }
 
-  Widget _buildSwitchTile(){
+  Widget _buildSwitchTile() {
     return SwitchListTile(
-                  value: _acceptterms,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _acceptterms = value;
-                    });
-                  },
-                  title: Text('Accept Terms'),
-                );
+      value: _acceptterms,
+      onChanged: (bool value) {
+        setState(() {
+          _acceptterms = value;
+        });
+      },
+      title: Text('Accept Terms'),
+    );
   }
 
-  void _submitForm(){
+  void _submitForm() {
     Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth =  MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 768.0 ? 500.0 : deviceWidth * 0.95;
     return Scaffold(
       appBar: AppBar(
         title: Text('Login'),
       ),
       body: Container(
-        decoration: BoxDecoration(
-            image: _buildBackgroundImage()),
+        decoration: BoxDecoration(image: _buildBackgroundImage()),
         padding: EdgeInsets.all(10.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                _buildEmailTextField(),
-                SizedBox(height: 10.0,),
-                _buildPasswordTextField(),
-                _buildSwitchTile(),
-                SizedBox(
-                  height: 10.0,
-                ),
-                RaisedButton(
-                  color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                  child: Text('LOGIN'),
-                  onPressed: _submitForm,
-                )
-              ],
+            child: Container(
+              width: targetWidth,
+              child: Column(
+                children: <Widget>[
+                  _buildEmailTextField(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  _buildPasswordTextField(),
+                  _buildSwitchTile(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    child: Text('LOGIN'),
+                    onPressed: _submitForm,
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -6,47 +6,44 @@ import '../ui_elements/title_default.dart';
 
 import './address_tag.dart';
 
-
-class ProductCard extends StatelessWidget{
-  final Map<String , dynamic> product;
+class ProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
   final int productindex;
 
-  ProductCard(this.product,this.productindex);
+  ProductCard(this.product, this.productindex);
 
-  Widget _buildPricerow(){
+  Widget _buildPricerow() {
     return Container(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TitleDefault(product['title']),
-                SizedBox(
-                  width: 8.0,
-                ),
-                PriceTag(product['price'].toString())
-              ],
-            ),
-          );
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TitleDefault(product['title']),
+          SizedBox(
+            width: 8.0,
+          ),
+          PriceTag(product['price'].toString())
+        ],
+      ),
+    );
   }
 
-  Widget _buildActionButtons(BuildContext context){
+  Widget _buildActionButtons(BuildContext context) {
     return ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.info),
-                  color: Theme.of(context).primaryColor,
-                  onPressed: () => Navigator.pushNamed<bool>(
-                      context, '/product/' + productindex.toString())
-                      ),
-              IconButton(
-                  icon: Icon(Icons.favorite_border),
-                  color: Colors.red,
-                  onPressed: () => Navigator.pushNamed<bool>(
-                      context, '/product/' + productindex.toString())
-                      ),
-            ],
-          );
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+            icon: Icon(Icons.info),
+            color: Theme.of(context).primaryColor,
+            onPressed: () => Navigator.pushNamed<bool>(
+                context, '/product/' + productindex.toString())),
+        IconButton(
+            icon: Icon(Icons.favorite_border),
+            color: Colors.red,
+            onPressed: () => Navigator.pushNamed<bool>(
+                context, '/product/' + productindex.toString())),
+      ],
+    );
   }
 
   @override
