e6f610393960928844471ab3c2b71712658936a7
basha97 <bashaadil2727@gmail.com>
2019-05-02 12:01:16 +0530

    Using scoped model in single products

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyAppState extends State<MyApp>{
         if (pathElements[1] == 'product') {
           final int index = int.parse(pathElements[2]);
           return MaterialPageRoute<bool>(
-                        builder: (BuildContext context) => ProductPage(null,null,null,null));
+                        builder: (BuildContext context) => ProductPage(index));
         }
       },
       onUnknownRoute: (RouteSettings settings){
--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,13 +4,18 @@ import 'package:flutter/material.dart';
 
 import '../widgets/ui_elements/title_default.dart';
 
-class ProductPage extends StatelessWidget {
-  final String title;
-  final String imageUrl;
-  final String description;
-  final double price;
+import 'package:scoped_model/scoped_model.dart';
 
-  Widget _buildAddressPriceRow(){
+import '../scoped-models/products.dart';
+
+import '../models/product.dart';
+
+class ProductPage extends StatelessWidget {
+  final int productIndex;
+
+  ProductPage(this.productIndex);
+
+  Widget _buildAddressPriceRow(double price){
     return Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -32,7 +37,7 @@ class ProductPage extends StatelessWidget {
   }
      
 
-  ProductPage(this.title, this.imageUrl,this.price,this.description);
+  
 
   @override
   Widget build(BuildContext context) {
@@ -42,28 +47,31 @@ class ProductPage extends StatelessWidget {
         Navigator.pop(context, false);
         return Future.value(false);
       },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: TitleDefault(title),
-            ),
-            _buildAddressPriceRow(),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              child: Text(
-                description,
-                textAlign: TextAlign.center,
-                ))
-          ],
-        ),
-      ),
+      child: ScopedModelDescendant<ProductsModel>(builder: (BuildContext context,Widget child,ProductsModel model){
+        final Product product = model.products[productIndex];
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(product.title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(product.image),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TitleDefault(product.title),
+              ),
+              _buildAddressPriceRow(product.price),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  ))
+            ],
+          ),
+        );
+      } ),
     );
   }
 }
